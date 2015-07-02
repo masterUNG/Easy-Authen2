c0eefa37fb182560e8679785456d2367a76f6090
masterUNG <phrombutr@gmail.com>
2015-07-03 00:38:55 +0700

    finish

--- Easy Authen2/ViewController.swift
+++ Easy Authen2/ViewController.swift
@@ -9,6 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTexrField: UITextField!
+    
+    
+    
+    @IBAction func loginButton(sender: AnyObject) {
+        
+        //Get Value From TextField
+        var strUser:String = userTextField.text
+        var srtPass:String = passwordTexrField.text
+        
+        checkSpace(strUser, argPass: srtPass)
+        
+        
+    }   // loginButton
+    
+    func checkSpace(argUser:String, argPass:String)->Void{
+    
+        //Test
+        println("User = \(argUser)")
+        println("Password = \(argPass)")
+        
+        if ((argUser.isEmpty) || (argPass.isEmpty)) {
+            myAlert()
+        }
+        else {
+            checkUserPass(argUser, Password: argPass)
+        }
+        
+        
+    } //checkSpace
+    
+    func myAlert()->Void{
+    
+        let myDialog = UIAlertController(title: "Have Space", message: "Please Fill every Blank", preferredStyle: UIAlertControllerStyle.Alert)
+        myDialog.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        
+        self.presentViewController(myDialog, animated: true, completion: nil)
+        
+    }
+    
+    func checkUserPass(User:String, Password:String)->Void{
+    
+        if ((User == "master") && (Password == "12345")) {
+            moveToMaster()
+        }
+        else {
+            myAlert2()
+        }
+        
+    } //checkUser
+    
+    func moveToMaster()->Void{
+    
+        let Master = self.storyboard?.instantiateViewControllerWithIdentifier("master") as! masterViewController
+        self.navigationController?.pushViewController(Master, animated: true)
+    
+    }
+    
+    func myAlert2()->Void{
+        
+        let myDialog = UIAlertController(title: "Login False", message: "Please Try Again", preferredStyle: UIAlertControllerStyle.Alert)
+        myDialog.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        
+        self.presentViewController(myDialog, animated: true, completion: nil)
+        
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +93,6 @@ class ViewController: UIViewController {
     }
 
 
-}
+}   // Main Class
+
 
